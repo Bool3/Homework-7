1cc8131f6675cd4eb4ce8f29c8ba1163a3bce919
Julian Sarkissian <pixlkid24@gmail.com>
2021-01-13 11:31:18 -0800

    finished hw 7

--- judgement.py
+++ judgement.py
@@ -1,15 +1,26 @@
 from extract_data import test_db
+import math
 
-def shared(student_a, student_b, database=test_db):
-    sid_a = 'student' + student_a
-    sid_b = 'student' + student_b
+def shared(studentID_a, studentID_b, database):
+    db_stu_a = database[studentID_a]
+    db_stu_b = database[studentID_b]
 
-    a = set(song for song in database[sid_a] if database[sid_a][song] != None)
-    b = set(song for song in database[sid_b] if database[sid_a][song] != None)
+    a = set(song for song in db_stu_a if db_stu_a[song] != None)
+    b = set(song for song in db_stu_b if db_stu_b[song] != None)
 
-    shared = a.intersection(b)
+    shared = list(a.intersection(b))
 
     return shared
 
-def euclidean(dictionary_key_studentId_value_ratings, studentId_1, studentId_2):
-    pass
+def euclidean(studentID_a, studentID_b, database):
+    both_rated = shared(studentID_a, studentID_b, database)
+    point_summation = 0
+
+    for song in both_rated:
+        point_summation += abs(database[studentID_a][song] - database[studentID_b][song]) ** 2
+    
+    return math.sqrt(point_summation)
+
+assert math.isclose(euclidean('student914', 'student9804', test_db), 5.0)
+assert math.isclose(euclidean('student5231', 'student4157', test_db), 3.0)
+assert math.isclose(euclidean('student1397', 'student7408', test_db), 4.3589, rel_tol=0.0009)
